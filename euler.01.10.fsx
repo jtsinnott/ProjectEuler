e395ce90b9ec7cd93d419e5cfee8cd0aa97a545f
John Sinnott <john@Johns-MacBook-Pro.local>
2016-07-09 20:21:49 -0700

    Split into one file per 10 problems.

---
+++ euler.01.10.fsx
@@ -0,0 +1,117 @@
+#load "utils.fs"
+#load "Seq.fs"
+#time
+
+open Euler.Utils
+open Euler.Seq
+open System
+
+(* https://projecteuler.net/archives *)
+
+(* PROBLEM 1 *)
+let p1 () =
+  [|1..999|]
+  |> Seq.filter (fun i -> i % 3 = 0 || i % 5 = 0)
+  |> Seq.sum
+
+let p1' () =
+  seq { for i in 1..999 do
+          if i % 3 = 0 || i % 5 = 0 then
+            yield i }
+  |> Seq.sum
+
+//p1 ()
+//p1' ()
+
+(* PROBLEM 2 *)
+let p2 () =
+  let rec fib (a : int) (b : int) = seq {
+    yield a
+    yield! (fib b (a + b))
+  }
+
+  fib 1 1
+  |> Seq.takeWhile (fun x -> x < 4000000)
+  |> Seq.where (fun x -> x % 2 = 0)
+  |> Seq.sum
+
+//p2 ()
+
+(* PROBLEM 3 *)
+let p3 () =
+  let n = 600851475143L
+  seq { 2L..(sqrt'(n) + 2L) }
+  |> Seq.filter (fun i -> n % i = 0L) // Filter by mod first.
+  |> Seq.filter isPrime               // Then by isPrime.
+  |> Seq.max
+
+//p3 ()
+
+(* PROBLEM 4 *)
+let p4 () =
+  seq { for i = 999 downto 100 do // Count down to get result faster.
+          for j = 999 downto 100 do
+            yield i * j }
+  |> Seq.filter (string >> isPalindrome)
+  |> Seq.take 1
+  |> Seq.exactlyOne
+
+//p4 ()
+
+(* PROBLEM 5 *)
+let p5 () =
+  let nums = seq { 2..20 }
+  let isDivisibleByAllNums x = nums |> Seq.forall (fun y -> x % y = 0)
+  // Here we increment in steps of <maxnum>
+  let max = Seq.max nums
+  Seq.unfold (fun x -> Some(x, x + max)) max
+  |> Seq.find isDivisibleByAllNums
+
+//p5 ()
+
+(* PROBLEM 6 *)
+let p6 () =
+  let sumOfSquares x = x |> Seq.sumBy (fun x -> x*x)
+  let squareOfSums x = let sumX = (Seq.sum x) in sumX * sumX
+  let first100N = seq { 1..100 }
+  abs((sumOfSquares first100N) - (squareOfSums first100N))
+
+//p6 ()
+
+(* PROBLEM 7 *)
+let p7 () =
+  Seq.unfold (fun i -> Some(i, i+1L)) 1L
+  |> Seq.filter isPrime
+  |> Seq.item 10000 // item is 0 based index
+
+//p7 ()
+
+(* PROBLEM 8 *)
+let p8 () =
+  System.IO.File.ReadAllText("p8.txt").ToCharArray()
+  |> Seq.windowed 13
+  |> Seq.map product
+  |> Seq.max
+
+//p8 ()
+
+(* PROBLEM 9 *)
+let p9 () =
+  seq { for a = 1 to 1000 do
+          for b = 1 to (1000-a) do
+            let c = 1000 - a - b
+            if a*a + b*b = c*c then
+              yield (a, b, c) }
+  |> Seq.distinctBy (fun (a, b, c) -> a + b + c)
+  |> Seq.map (fun (a, b, c) -> a * b * c)
+  |> List.ofSeq
+
+//p9 ()
+
+(* PROBLEM 10 *)
+let p10 () =
+  seq { 2L..(2000000L-1L) }
+  |> Seq.filter isPrime
+  |> Seq.sum
+
+//p10 ()
---
+++ euler.11.20.fsx
@@ -0,0 +1,119 @@
+#load "utils.fs"
+#load "Seq.fs"
+#time
+
+open Euler.Utils
+open Euler.Seq
+open System
+
+(* https://projecteuler.net/archives *)
+
+(* PROBLEM 11 *)
+type Direction = Down | Right | DownRight | DownLeft
+let p11 () =
+  let grid =
+    let digits = System.IO.File.ReadAllText("p11.txt")
+    digits.Split('\n')
+    |> Array.map (fun l -> l.Split(' ') |> Array.map int)
+
+  let max_i, max_j = grid.Length - 1, grid.[0].Length - 1
+
+  let gridVec i j n d =
+    let up i n = [| i..(-1)..(i - (n-1)) |]
+    let dn i n = [| i..(i + (n-1)) |]
+    let lt j n = [| j..(-1)..(j - (n-1)) |]
+    let rt j n = [| j..(j + (n-1)) |]
+    let stay (x : int) n = Array.replicate n x
+    let f (i, j) = i <= max_i && i > 0 && j <= max_j && j > 0
+    let vec = function
+      | Down      -> Array.zip (dn i n) (stay j n)
+      | Right     -> Array.zip (stay i n) (rt j n)
+      | DownRight -> Array.zip (dn i n) (rt j n)
+      | DownLeft  -> Array.zip (dn i n) (lt j n)
+    d |> vec |> Array.filter f
+
+  let gridVecProd i j n d =
+    gridVec i j n d
+    |> Array.map (fun (i,j) -> grid.[i].[j])
+    |> (fun xs -> if (Array.isEmpty xs) then 0 else Array.reduce (*) xs)
+
+  seq {
+    for i in 0..max_i do
+      for j in 0..max_j do
+        for d in [Down; Right; DownLeft; DownRight] do
+          yield gridVecProd i j 4 d
+  } |> Seq.max
+
+//p11 ()
+
+(* PROBLEM 12 *)
+let p12 () =
+  let triangleNum n = [1L..n] |> Seq.sum
+  let naturalNums = Seq.unfold (fun i -> Some (triangleNum i, i+1L)) 1L
+
+  naturalNums
+  |> Seq.map (fun t -> (t, Seq.length (divisors t)))
+  |> Seq.find (fun (_, l) -> l > 500)
+
+//p12 ()
+
+(* PROBLEM 13 *)
+let p13() =
+  System.IO.File.ReadAllLines("p13.txt")
+  |> Array.map (fun x -> Int64.Parse (x.Substring(0, 11)))
+  |> Array.sum
+  |> (fun x -> (string x).Substring(0, 10))
+
+//p13 ()
+
+(* PROBLEM 14 *)
+let p14 () =
+  let nextCollatz n = if n % 2L = 0L then n/2L else 3L*n + 1L
+  let rec collatzSeqLength = function
+    | 1L -> 1L
+    | n  -> 1L + (collatzSeqLength (nextCollatz n)) // Not tail recursive.
+  seq { 1L..999999L } |> Seq.maxBy collatzSeqLength
+
+//p14 ()
+
+(* PROBLEM 15 *)
+let p15 () =
+  let rec fac n = if n <= 1I then 1I else fac(n-1I) * n
+  // combination (n sub k) = n! / (k! * (n-k)!)
+  let combo n k = (fac n) / (fac k * (fac (n-k)))
+  combo 40I 20I
+
+//p15 ()
+
+(* PROBLEM 16 *)
+let p16 () =
+  (string (pown (bigint 2) 1000)).ToCharArray()
+  |> Array.sumBy (string >> Int32.TryParse >> snd)
+
+//p16 ()
+
+(* PROBLEM 17 *)
+let p17() =
+  let rec numWord = function
+    | 0  -> "" | 1 -> "one" | 2 -> "two" | 3 -> "three" | 4 -> "four"
+    | 5  -> "five" | 6 -> "six" | 7 -> "seven" | 8 -> "eight" | 9 -> "nine"
+    | 11 -> "eleven" | 12 -> "twelve" | 13 -> "thirteen" | 14 -> "fourteen"
+    | 15 -> "fifteen" | 16 -> "sixteen" | 17 -> "seventeen" | 18 -> "eighteen"
+    | 19 -> "nineteen"
+    | 10 -> "ten" | 20 -> "twenty" | 30 -> "thirty" | 40 -> "fourty"
+    | 50 -> "fifty" | 60 -> "sixty" | 70 -> "seventy" | 80 -> "eighty"
+    | 90 -> "ninety"
+    | 1000 -> "one thousand"
+    | n when n / 100 >= 1 ->
+      let hundreds = numWord (n / 100)
+      let rest = numWord (n % 100)
+      sprintf "%s hundred and %s" hundreds rest
+    | n when n / 10 > 1 ->
+      let tens = numWord (n - (n % 10))
+      let ones = numWord (n % 10)
+      sprintf "%s-%s" tens ones
+    | n -> sprintf "unknown number %d" n
+
+  [1..1000] |> List.sumBy (numWord >> String.length)
+
+//p17 ()
